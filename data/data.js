726b27600a785c6a25d0800bbfaf9826de57e879
Martin Alberti <martinalberti123@gmail.com>
2022-10-06 16:44:47 -0300

    no anda el form

--- data/data.js
+++ data/data.js
@@ -5,7 +5,7 @@ const products = [
     price: 323.45,
     thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Squadra_45.jpg',
     description:"Escuadra de plastico",
-    stock: "10"
+    stock: 10
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const products = [
     price: 234.56,
     thumbnail: 'https://micalculadoracientifica.com/wp-content/uploads/2021/01/TI-Nspire-CX-Amazon.jpg', 
     description:"Calculadora cientifica",
-    stock: "10"
+    stock: 10
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const products = [
     price: 45.67,
     thumbnail: 'https://panamericana.vteximg.com.br/arquivos/ids/256800-600-690/globo-terraqueo-politico-40-cm-7701016736787.jpg?v=636381897120030000',
     description:"Globo redondo",
-    stock: "10"
+    stock: 10
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const products = [
     price: 456.78,
     thumbnail: 'https://www.botiga.com.uy/media/catalog/product/cache/1/image/600x600/0dc2d03fe217f8c83829496872af24a0/p/a/paleta_pintora_tempera_infantozzi_materiales.jpg',
     description:"Paleta de pintura profesional",
-    stock: "10"
+    stock: 10
   },
 ];
 
